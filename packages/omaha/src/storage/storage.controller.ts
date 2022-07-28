2e16901b20269602381a86a605c6a1dc23e01d76
Bailey Herbert <hello@bailey.sh>
2022-07-28 01:35:26 -0700

    Use status code `403` for download rejections

--- packages/omaha/src/storage/storage.controller.ts
+++ packages/omaha/src/storage/storage.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Controller, Get, NotFoundException, Param, Query, StreamableFile } from '@nestjs/common';
+import { Controller, ForbiddenException, Get, NotFoundException, Param, Query, StreamableFile } from '@nestjs/common';
 import { Guest } from 'src/auth/decorators/guest.decorator';
 import { DownloadQueryDto } from './dto/DownloadQueryDto';
 import { StorageService } from './storage.service';
@@ -21,20 +21,20 @@ export class StorageController {
 		const expiration = Number(query.expires);
 
 		if (!(driver instanceof LocalStorageDriver)) throw new NotFoundException();
-		if (isNaN(expiration)) throw new BadRequestException('Invalid expiration timestamp');
-		if (Date.now() >= (expiration * 1000)) throw new BadRequestException('Download link has expired');
+		if (isNaN(expiration)) throw new ForbiddenException('Invalid expiration timestamp');
+		if (Date.now() >= (expiration * 1000)) throw new ForbiddenException('Download link has expired');
 
 		const signature = driver.getDownloadSignature(objectName, query.expires, query.disposition);
 
 		if (signature !== query.signature) {
-			throw new BadRequestException('Incorrect signature');
+			throw new ForbiddenException('Incorrect signature');
 		}
 
 		const objectPath = path.resolve(driver.storagePath, objectName);
 		const objectStats = await stat(objectPath);
 
 		if (!objectStats) {
-			throw new NotFoundException('Object not found');
+			throw new ForbiddenException('Object not found');
 		}
 
 		const stream = fs.createReadStream(objectPath);
