dc61dda11e3c6940b75f053edc20a4c7c8189657
Bailey Herbert <hello@bailey.sh>
2022-08-04 19:44:49 -0700

    Add incremental versioning scheme

---
+++ packages/omaha/src/drivers/versions/IncrementalVersionDriver.ts
@@ -0,0 +1,64 @@
+import { BadRequestException } from '@nestjs/common';
+import { VersionList, VersionSchemeDriver } from '../interfaces/VersionSchemeDriver';
+
+/**
+ * This driver accepts versions in any format. It considers each new release to be an incremental update over the
+ * release before it. Thus, unlike other version schemes, it does not have a concept of major or minor versioning, and
+ * it is not possible to create a release for an older version.
+ */
+export class IncrementalVersionDriver implements VersionSchemeDriver {
+
+	public validateVersionString(input: string) {
+		input = input.trim();
+
+		if (input.length === 0) {
+			throw new Error('Invalid version string');
+		}
+
+		return input;
+	}
+
+	public getVersionsFromConstraint(versions: VersionList, constraint: string) {
+		const matches = constraint.match(/^(>=|>|<=|<|\^)?(.+)$/);
+
+		if (!matches) {
+			throw new BadRequestException(`The string '${constraint}' is not a valid version constraint`);
+		}
+
+		const targetOperator = matches[1];
+		const targetVersion = matches[2];
+		const targetIndex = versions.all.indexOf(targetVersion);
+		let eligibleVersions: Set<string>;
+
+		if (targetIndex < 0) {
+			return [];
+		}
+
+		// Narrow down the eligible versions from the 'all' array bsaed on the target index
+		switch (targetOperator) {
+			case '^': case '>=': eligibleVersions = new Set(versions.all.slice(targetIndex)); break;
+			case '>': eligibleVersions = new Set(versions.all.slice(targetIndex + 1)); break;
+			case '<=': eligibleVersions = new Set(versions.all.slice(0, targetIndex + 1)); break;
+			case '<': eligibleVersions = new Set(versions.all.slice(0, targetIndex)); break;
+			case undefined: eligibleVersions = new Set(versions.all.slice(targetIndex, targetIndex + 1));
+		}
+
+		// Return versions within the 'selected' array that are eligible
+		return versions.selected.filter(version => eligibleVersions.has(version));
+	}
+
+	public getVersionsSorted(versions: VersionList, direction: 'asc' | 'desc') {
+		const selected = [...versions.selected];
+
+		// Sort the selection in ascending order (using the 'all' array)
+		selected.sort((a, b) => versions.all.indexOf(a) - versions.all.indexOf(b));
+
+		// Reverse the order for descending
+		if (direction === 'desc') {
+			selected.reverse();
+		}
+
+		return selected;
+	}
+
+}
--- packages/omaha/src/drivers/versions/index.ts
+++ packages/omaha/src/drivers/versions/index.ts
@@ -1,11 +1,12 @@
 import { RepositoryVersionScheme } from 'src/entities/enum/RepositoryVersionScheme';
 import { VersionSchemeDriver } from '../interfaces/VersionSchemeDriver';
+import { IncrementalVersionDriver } from './IncrementalVersionDriver';
 import { MicrosoftVersionDriver } from './MicrosoftVersionDriver';
 import { SemanticVersionDriver } from './SemanticVersionDriver';
 
 export const VersionSchemeDrivers: Record<RepositoryVersionScheme, VersionSchemeDriver> = {
 	semantic: new SemanticVersionDriver(),
 	calendar: null,
-	incremental: null,
+	incremental: new IncrementalVersionDriver(),
 	microsoft: new MicrosoftVersionDriver(),
 };
