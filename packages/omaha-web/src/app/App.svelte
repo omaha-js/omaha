75d18c93c5e3f6e1a47abc6e55ce0bf8930a792b
Bailey Herbert <hello@bailey.sh>
2022-08-15 19:23:57 -0700

    Fetch full repository before loading routes

--- packages/omaha-web/src/app/App.svelte
+++ packages/omaha-web/src/app/App.svelte
@@ -9,6 +9,8 @@
 	import Layout from './components/layouts/Layout.svelte';
 	import NotificationTray from './components/layouts/notifications/NotificationTray.svelte';
 	import { Repository } from '@omaha/client';
+	import PromiseLoader from './components/helpers/PromiseLoader.svelte';
+import { client } from 'src/omaha/globals';
 
 	const { account } = omaha.session;
 
@@ -16,6 +18,10 @@
 	const { repositories } = omaha.repositories;
 
 	let repository: Repository | undefined;
+	let repositoryPromise: Promise<Repository> | undefined;
+	let repositoryPromiseId = '';
+
+	const [client] = omaha.client.useFromComponent();
 
 	$: {
 		const match = $router.path.match(regex);
@@ -26,6 +32,11 @@
 
 			if (repository !== active) {
 				repository = active;
+			}
+
+			if (active && repositoryPromiseId !== active.id) {
+				repositoryPromiseId = active.id;
+				repositoryPromise = client.repos.get(repositoryPromiseId);
 			}
 		}
 		else if (repository !== undefined) {
@@ -42,18 +53,32 @@
 	<Layout {repository}>
 		<Route path="/">Admin</Route>
 
-		<!-- Repositories -->
+		<!-- Repository management (account side) -->
 		<Route path="/repositories/create"><Loadable component={ () => import('./pages/repositories/create.svelte') } /></Route>
-		<Route path="/repository/:repo_id/*">
-			{#if repository}
-			<Route path="/" redirect="releases" />
-			<Route path="/releases"><LoadableForRepo repo={repository} component={ () => import('./pages/repositories/releases.svelte') } /></Route>
-			<Route path="/assets"><LoadableForRepo repo={repository} component={ () => import('./pages/repositories/assets.svelte') } /></Route>
-			<Route path="/tags"><LoadableForRepo repo={repository} component={ () => import('./pages/repositories/tags.svelte') } /></Route>
-			<Route path="/stats"><LoadableForRepo repo={repository} component={ () => import('./pages/repositories/stats.svelte') } /></Route>
-			<Route path="/settings/*"><LoadableForRepo repo={repository} component={ () => import('./pages/repositories/settings.svelte') } /></Route>
-			{/if}
-		</Route>
+
+		<!-- Repositories -->
+		{#if repository && repositoryPromise}
+			<PromiseLoader promise={repositoryPromise} let:value={repository}>
+				<Route path="/repository/:repo_id/*">
+					<Route path="/" redirect="releases" />
+					<Route path="/releases">
+						<LoadableForRepo repo={repository} component={ () => import('./pages/repositories/releases.svelte') } />
+					</Route>
+					<Route path="/assets">
+						<LoadableForRepo repo={repository} component={ () => import('./pages/repositories/assets.svelte') } />
+					</Route>
+					<Route path="/tags">
+						<LoadableForRepo repo={repository} component={ () => import('./pages/repositories/tags.svelte') } />
+					</Route>
+					<Route path="/stats">
+						<LoadableForRepo repo={repository} component={ () => import('./pages/repositories/stats.svelte') } />
+					</Route>
+					<Route path="/settings/*">
+						<LoadableForRepo repo={repository} component={ () => import('./pages/repositories/settings.svelte') } />
+					</Route>
+				</Route>
+			</PromiseLoader>
+		{/if}
 
 		<!-- Account -->
 		<Route path="/account/repositories"><Loadable component={ () => import('./pages/protected/account/repositories/index.svelte') } /></Route>
