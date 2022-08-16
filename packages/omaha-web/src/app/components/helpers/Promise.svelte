e2047900bcfa5d770599f28e6b31155d95f136cd
Bailey Herbert <hello@bailey.sh>
2022-08-15 17:31:50 -0700

    Add a component for promises

---
+++ packages/omaha-web/src/app/components/helpers/Promise.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Loader from './Loader.svelte';
+	import AlertTriangle from 'tabler-icons-svelte/icons/AlertTriangle.svelte';
+
+	type T = $$Generic;
+	export let promise: Promise<T>;
+</script>
+
+{#await promise}
+	<div class="promise loading">
+		<Loader size={48} color='gray' />
+	</div>
+{:then value}
+	<slot {value} />
+{:catch error}
+	<div class="promise error">
+		<div class="inner">
+			<AlertTriangle />
+			<p>{error.message}</p>
+		</div>
+	</div>
+{/await}
