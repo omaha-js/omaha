d476602425bd182ab6f289658260450b4c017e89
Bailey Herbert <hello@bailey.sh>
2022-08-18 15:08:52 -0700

    Remove stats from repo navigation

--- packages/omaha-web/src/app/components/layouts/header/HeaderRepoNavigation.svelte
+++ packages/omaha-web/src/app/components/layouts/header/HeaderRepoNavigation.svelte
@@ -19,7 +19,6 @@
 				<li><a href="/repository/{repository.id}/releases" use:active={repository}>Releases</a></li>
 				<li><a href="/repository/{repository.id}/assets" use:active={repository}>Assets</a></li>
 				<li><a href="/repository/{repository.id}/tags" use:active={repository}>Tags</a></li>
-				<li><a href="/repository/{repository.id}/stats" use:active={repository}>Stats</a></li>
 				<li><a href="/repository/{repository.id}/settings" use:active={repository}>Settings</a></li>
 			</ul>
 		</nav>
