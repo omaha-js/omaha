67f5843f78ab012c835728473bf524e9d238c486
Bailey Herbert <hello@bailey.sh>
2022-08-17 19:36:36 -0700

    Add a gravatar component

---
+++ packages/omaha-web/src/app/components/helpers/Gravatar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import md5 from 'blueimp-md5';
+
+	export let email: string;
+	export let alt: string;
+	export let size: number;
+
+	let avatarURL = '';
+
+	$: {
+		let hash = md5(email.trim().toLowerCase());
+		avatarURL = `https://www.gravatar.com/avatar/${hash}?d=mp&r=g&s=${size}`;
+	}
+</script>
+
+<img src={avatarURL} {alt} {...$$restProps} />
--- packages/omaha-web/src/app/components/layouts/header/HeaderUserDropdown.svelte
+++ packages/omaha-web/src/app/components/layouts/header/HeaderUserDropdown.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
-	import md5 from 'blueimp-md5';
 	import omaha from 'src/omaha';
-
+	import Gravatar from '../../helpers/Gravatar.svelte';
 	const { account } = omaha.session;
-
-	let avatarURL = '';
-
-	$: {
-		let hash = md5($account!.email.trim().toLowerCase());
-		avatarURL = `https://www.gravatar.com/avatar/${hash}?d=mp&r=g&s=100`;
-	}
 </script>
 
 <div class="header-user">
 	<div class="avatar">
-		<img src={avatarURL} alt={$account?.name} />
+		{#if $account}
+			<Gravatar email={$account.email} alt={$account.name} size={100} />
+		{/if}
 	</div>
 </div>
