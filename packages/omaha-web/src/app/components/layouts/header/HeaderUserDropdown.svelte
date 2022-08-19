385e7eacab29348200db2f3810bd590b3b88797c
Bailey Herbert <hello@bailey.sh>
2022-08-18 22:18:23 -0700

    Add dropdown menu for account

--- packages/omaha-web/src/app/components/layouts/header/HeaderUserDropdown.svelte
+++ packages/omaha-web/src/app/components/layouts/header/HeaderUserDropdown.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
 	import omaha from 'src/omaha';
+	import { ButtonDropdown, DropdownItem, DropdownMenu } from 'sveltestrap';
 	import Gravatar from '../../helpers/Gravatar.svelte';
+	import DropdownToggle from '../../kit/DropdownToggle.svelte';
 	const { account } = omaha.session;
 </script>
 
 <div class="header-user">
-	<div class="avatar">
-		{#if $account}
-			<Gravatar email={$account.email} alt={$account.name} size={100} />
-		{/if}
-	</div>
+	<ButtonDropdown class="dropdown">
+		<DropdownToggle>
+			<div class="avatar">
+				{#if $account}
+					<Gravatar email={$account.email} alt={$account.name} size={100} />
+				{/if}
+			</div>
+		</DropdownToggle>
+		<DropdownMenu end={true}>
+			<DropdownItem href="/account/settings">Settings</DropdownItem>
+			<DropdownItem href="/account/logout">Sign out</DropdownItem>
+		</DropdownMenu>
+	</ButtonDropdown>
 </div>
