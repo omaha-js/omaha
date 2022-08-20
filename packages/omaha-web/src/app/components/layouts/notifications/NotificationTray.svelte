5c9c76874b3ee808ca509c7059727dbb0d468ff1
Bailey Herbert <hello@bailey.sh>
2022-08-19 17:01:52 -0700

    Disable pointer events on fading notifications

--- packages/omaha-web/src/app/components/layouts/notifications/NotificationTray.svelte
+++ packages/omaha-web/src/app/components/layouts/notifications/NotificationTray.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
-import omaha from 'src/omaha';
-
-	import { fly } from 'svelte/transition';
+	import omaha from 'src/omaha';
+	import { fly, FlyParams } from 'svelte/transition';
 	import Notification from './Notification.svelte';
 
 	const { alerts } = omaha.alerts;
+
+	function flyfade(node: Element, params: FlyParams) {
+		node.classList.add('notification-fading');
+		return fly(node, params);
+	}
 </script>
 
 <div class="notifications">
 	{#each $alerts as alert (alert.id)}
-		<div in:fly="{{ y: -200, duration: 300 }}" out:fly="{{ y: 200, duration: 600 }}">
+		<div in:fly="{{ y: -200, duration: 300 }}" out:flyfade="{{ y: 200, duration: 600 }}">
 			<Notification
 				title={alert.title}
 				message={alert.message}
