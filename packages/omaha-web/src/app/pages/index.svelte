8846b1d572c433033d653e6ffa86d50074112150
Bailey Herbert <hello@bailey.sh>
2022-08-22 18:11:17 -0700

    Implement dashboard

--- packages/omaha-web/src/app/pages/index.svelte
+++ packages/omaha-web/src/app/pages/index.svelte
@@ -1,11 +1,58 @@
-<script>
+<script lang="ts">
+	import { RepositoryAccessType } from '@omaha/client';
 	import omaha from 'src/omaha';
+	import { onDestroy } from 'svelte';
+	import PromiseLoader from '../components/helpers/PromiseLoader.svelte';
+	import Time from '../components/helpers/Time.svelte';
+	import PublicIcon from 'tabler-icons-svelte/icons/Notebook.svelte';
+	import PrivateIcon from 'tabler-icons-svelte/icons/Lock.svelte';
 
-	const { account } = omaha.session;
+	const [client, error, loading, dispose] = omaha.client.useFromComponent();
+	onDestroy(dispose);
+
+	let promise = client.repos.overview();
 </script>
 
 <svelte:head><title>{omaha.app.title('Dashboard')}</title></svelte:head>
 
-Hello {$account.name}!
+<PromiseLoader {promise} let:value={results}>
+	<div class="repo-list-heading">
+		<h1>Repositories</h1>
+		<div class="count">{results.length}</div>
+	</div>
 
-<button on:click={ () => omaha.session.clear() }>Sign out</button>
+	<div class="dashboard-list">
+		{#each results as { repository, collaboration, downloads, release }}
+			<a href="/repository/{repository.id}" class="repo-list-item">
+				<div class="repo-overview">
+					<div class="repo-name">
+						<div>
+							{#if repository.access === RepositoryAccessType.Public}
+								<PublicIcon />
+							{:else}
+								<PrivateIcon />
+							{/if}
+						</div>
+						<div>{repository.name}</div>
+					</div>
+					<div class="repo-description">{repository.description}</div>
+					<div class="repo-details">
+						<div class="repo-version">
+							{#if release}
+								<span class="version-tag">{release.version}</span>
+								<span class="sep">·</span>
+								<span><Time timestamp={release.published_at} /></span>
+							{:else}
+								<span>Not published</span>
+							{/if}
+						</div>
+					</div>
+				</div>
+			</a>
+		{:else}
+			<p class="no-repositories">
+				You're not in any repositories yet. Why not <a href="/repositories/create">create one</a>?
+			</p>
+		{/each}
+	</div>
+</PromiseLoader>
